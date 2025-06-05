330c7078d632240033fc0eadf1dafb3e0500ba8c
davor <santiago.carbo05@gmail.com>
2025-06-05 19:29:32 -0300

    Update client 1

---
+++ src/clients/client.repository.ts
@@ -0,0 +1,18 @@
+import { Cliente } from "./client.entity.js";
+import { Repository } from "../shared/repository.js";
+
+const clientes= [new Cliente(
+  'Juan',
+  'Password1234',
+  'juan123@gmail.com',
+  '549-341-244-2356',
+  46392822,
+  'a02b91bc-3769-4221-beb1-d7a3aeba7dad'
+)
+]
+
+export class ClientRepository implements Repository<Cliente>{
+  public async findAll(): Promise<Cliente[] | undefined> {
+    return await clientes
+  }
+}
