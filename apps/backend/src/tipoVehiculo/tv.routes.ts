2da48160c8960db0f3e16323c9a46f6fbbcbc14b
unknown <felipemuntaabski@gmail.com>
2025-07-07 17:43:29 -0300

    CRUD tv

---
+++ apps/backend/src/tipoVehiculo/tv.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { TipoVehiculo } from "./tv.entity.js"
+import {findAll, findOne, add, update, remove, sanitizedTipoVehiculoInput} from "./tv.controler.js" 
+export const TipoVehiculoRouter = Router()
+
+TipoVehiculoRouter.get('/', findAll)
+TipoVehiculoRouter.get('/:id', findOne)
+TipoVehiculoRouter.post('/', sanitizedTipoVehiculoInput, add)
+TipoVehiculoRouter.put('/:id', sanitizedTipoVehiculoInput, update)
+TipoVehiculoRouter.delete('/:id', remove)
