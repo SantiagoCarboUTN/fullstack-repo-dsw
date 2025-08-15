69184e5b57f20c88e3e6f63f25c8d52f3b280c5b
davor <santiago.carbo05@gmail.com>
2025-08-15 12:06:48 -0300

    add cuota crud

--- apps/backend/dist/app.js
+++ apps/backend/dist/app.js
@@ -5,6 +5,7 @@ import { TipoServicioRouter } from "./tipoServicio/tserv.routes.js";
 import { VehiculoRouter } from "./vehiculo/vehiculo.routes.js";
 import { CocheraRouter } from "./cochera/cochera.routes.js";
 import { ReservaRouter } from "./reserva/reserva.routes.js";
+import { CuotaRouter } from "./cuotas/cuotas.routes.js";
 const app = express();
 app.use(express.json());
 app.use("/api/clients", Clientrouter);
@@ -13,6 +14,7 @@ app.use("/api/tipoServicio", TipoServicioRouter);
 app.use("/api/vehiculo", VehiculoRouter);
 app.use("/api/cochera", CocheraRouter);
 app.use("/api/reserva", ReservaRouter);
+app.use("/api/cuota", CuotaRouter);
 app.listen(3000, () => {
     console.log('Server runnning on http://localhost:3000/');
 });
---
+++ apps/backend/src/cuotas/cuotas.controller.ts
@@ -0,0 +1,59 @@
+
+import { Request, Response, NextFunction } from 'express'
+import { Cuota } from "./cuotas.entity.js";
+import { CuotaRepository } from "./cuotas.repository.js";
+const repository = new CuotaRepository()
+function sanitizedCuotaInput(req: Request, res: Response, next: NextFunction) {
+  req.body.sanitizedCuotaInput = {
+  fecha: req.body.fecha,
+  estado: req.body.estado,
+  id: req.body.id
+}
+Object.keys(req.body.sanitizedCuotaInput).forEach((key) => {
+  if (req.body.sanitizedCuotaInput[key] === undefined) {
+    delete req.body.sanitizedCuotaInput[key]
+  } 
+})
+next()
+}
+
+async function findAll(req: Request, res: Response) {
+  res.json({ data: await repository.findAll() })
+}
+async function findOne(req: Request, res: Response) {
+  const id = req.params.id
+  const cuota = await repository.findOne({id})
+  if (!cuota) {
+    return res.status(404).json({ error: 'No se encontró la cuota' })
+  }
+  res.json({ data: cuota })
+}
+async function add(req: Request, res: Response) {
+  const input = req.body.sanitizedCuotaInput
+
+  const cuotaInput = new Cuota(
+    input.fecha,
+    input.estado,
+    input.id
+  )
+  const cuota = await repository.add(cuotaInput)
+  return res.status(201).json({message: 'Se creó la cuota', data: cuota })
+} 
+async function update(req: Request, res: Response) {
+  req.body.sanitizedCuotaInput.id = req.params.id
+  const cuota = await repository.update(req.body.sanitizedCuotaInput)
+  if (!cuota) {
+    return res.status(404).json({ error: 'No se encontró la cuota' })
+  }
+  return res.status(200).json({message: 'Se actualizó la cuota', data: cuota })
+}
+async function remove(req: Request, res: Response) {
+  const id = req.params.id
+  const cuota = await repository.delete({id})
+  if (!cuota) {
+    return res.status(404).json({ error: 'No se encontró la cuota' })
+  }else {
+    return res.status(200).json({message: 'Se eliminó la cuota', data: cuota})
+  }
+}  
+export { findAll, findOne, add, update, remove, sanitizedCuotaInput};
