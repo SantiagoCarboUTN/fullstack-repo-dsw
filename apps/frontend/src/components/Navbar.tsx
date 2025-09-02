bff6984146064d52d910230e36bc8270d82950ca
ValentinCotorruelo <valentincotorruelo@gmail.com>
2025-09-02 19:35:39 -0300

    cambios en NavBar

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import { Default_Link } from "./default_link"
+import brand_logo from '../assets/brand_logo.svg'
 
 export const Navbar = () => {
   return (
-    <div  className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 
-    py-4 text-gray-600 border-b border-borderColor relative transition-all max-sm:fixed max-sm:h-screen max-sm:w-full 
-      max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col 
-      sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4`}>
+    <nav className='"w-full bg-white border-b border-borderColor py-0'>
+    <div  className="flex justify-center items-center gap-7  text-lg">
 
+        <Default_Link route='/'  isImg={true} imgSrc = {brand_logo}/>
         <Default_Link route="/" text="Home" />
         <Default_Link route="/reservas" text="Reservar cochera" />
         <Default_Link route="/pago" text="Realizar pago" />
         <Default_Link route="/singUp" text="Registrarse" />
         <Default_Link route="/login" text="Iniciar Sesión" />
     </div>
+    </nav>
     
   )
 }
--- apps/frontend/src/components/default_link.tsx
+++ apps/frontend/src/components/default_link.tsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom'
 
 type DefaultLinkProps = {
-  route: string
-  text: string
+  route: string,
+  text?: string,
+  isImg?: boolean,
+  imgSrc?: string
 }
+{/* El signo de pregunta en typescript significa que la propiedad es opcional */}
 
-export const Default_Link = ({ route, text }: DefaultLinkProps) => {
-  return (
+export const Default_Link = ({ route, text, isImg, imgSrc }: DefaultLinkProps) => {
+  return isImg ? (
+    <Link to={route} className="hover:scale-105 transform transition duration-200">
+      <img src={imgSrc} alt="Logo" className="h-25 w-auto" />
+      {text}
+    </Link>
+  ) : (
     <Link to={route} className="hover:text-black hover:scale-105 transform transition duration-200">
       {text}
     </Link>
