75bc4c9ebc45f44c6066ec3ab99418d59b1e73cf
davor <santiago.carbo05@gmail.com>
2025-10-07 16:50:19 -0300

    add dotenv dependencie

--- apps/backend/src/shared/db/conn.ts
+++ apps/backend/src/shared/db/conn.ts
@@ -3,9 +3,14 @@ import 'dotenv/config'
 export const pool = mysql.createPool({
  uri: process.env.MYSQL_URL
 })
-async function testDB():Promise<void> {
-  const [rows] = await pool.query('SELECT NOW() as now');
+
+/* async function testDB():Promise<void> {
+  const [rows] = await pool.query(`CREATE TABLE IF NOT EXISTS tipo_vehiculo (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(40)
+  );`);
   console.log(rows);
 }
 
 testDB();
+console.log(process.env.MYSQL_URL) */
