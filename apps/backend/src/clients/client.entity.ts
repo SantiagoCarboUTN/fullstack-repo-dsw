99841457caa12fc43f2601044edeea953f600026
unknown <felipemuntaabski@gmail.com>
2025-10-08 16:51:24 -0300

    add client &tserv

--- apps/backend/src/clients/client.entity.ts
+++ apps/backend/src/clients/client.entity.ts
@@ -1,1 +1,24 @@
-import {Entity,Property,OneToMany} from "@mikro-orm/core";
+import {Entity,Property,OneToMany, Cascade, Collection} from "@mikro-orm/core";
+import { BaseEntity } from "../shared/db/baseEntity.entity";
+import { Vehiculo } from "../vehiculo/vehiculo.entity.js";
+@Entity()
+export class Client extends BaseEntity {
+  @Property({nullable:false})
+    mail!: string;
+
+  @Property({nullable:false})
+    complete_name!: string;
+
+  @Property({nullable:false})
+    password!: string;
+
+  @Property({nullable:true})
+    phone!: string;
+
+  @Property({nullable:true})
+    dni!: string;
+
+    
+  @OneToMany (()=>Vehiculo, (vehiculo)=>vehiculo.client, {cascade:[Cascade.PERSIST, Cascade.REMOVE]})
+    vehiculos = new Collection<Vehiculo>(this);
+}
--- apps/backend/src/tipoServicio/tserv.entity.ts
+++ apps/backend/src/tipoServicio/tserv.entity.ts
@@ -1,8 +1,18 @@
-import crypto from 'node:crypto'
-export class TipoServicio {
-  constructor(
-    public nombre: string,
-    public precio: number,
-    public id = crypto.randomUUID()
-  ) {}
+import { Property, Entity, OneToMany } from "@mikro-orm/core";
+import { BaseEntity } from "../shared/db/baseEntity.entity.js";
+import { Reserva } from "../reserva/reserva.entity.js";
+
+/* id
+nombre
+precio */
+@Entity()
+export class tipoServicio extends BaseEntity {
+  @Property({ nullable: false })
+  nombre!: string;
+
+  @Property({ nullable: false })
+  precio!: number;
+
+  @OneToMany(() => Reserva, (reserva) => reserva.tipoServicio)
+  reservas!: Reserva[];
 }
