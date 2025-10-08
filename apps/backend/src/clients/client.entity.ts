9ea10699f92c6b6fdd4822f5f86dcd51b9ed5cb2
unknown <felipemuntaabski@gmail.com>
2025-10-08 16:21:01 -0300

    add mikro-orm/core

--- apps/backend/src/clients/client.entity.ts
+++ apps/backend/src/clients/client.entity.ts
@@ -1,12 +1,1 @@
-import crypto from 'node:crypto'
-export class Client {
-  constructor(
-    public name:string, 
-    public contraseña:string, 
-    public mail:string, 
-    public telefono:string, 
-    public dni : number,
-    public id = crypto.randomUUID()
-  ){}
-}
-
+import {Entity,Property,OneToMany} from "@mikro-orm/core";
