c5ff6b6da5b29c9081fe3c9f689b25a560705b8d
davor <santiago.carbo05@gmail.com>
2025-10-08 16:12:23 -0300

    add dotenv dependencie

--- apps/backend/src/shared/db/orm.ts
+++ apps/backend/src/shared/db/orm.ts
@@ -1,5 +1,6 @@
 import { MikroORM } from "@mikro-orm/mysql";
 import { SqlHighlighter} from "@mikro-orm/sql-highlighter";
+import 'dotenv/config'
 export const orm = await MikroORM.init({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
