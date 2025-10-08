340f58552fcc697347bd02d94bc6e454b24300e0
unknown <felipemuntaabski@gmail.com>
2025-10-08 17:28:16 -0300

    add cuotas.entity

--- apps/backend/src/cuotas/cuotas.entity.ts
+++ apps/backend/src/cuotas/cuotas.entity.ts
@@ -1,8 +1,19 @@
-import crypto from 'node:crypto'
-export class Cuota{
-  constructor(
-    public fecha: string,
-    public estado:string,
-    public id = crypto.randomUUID()
-  ){}
+import { Entity, Property, PrimaryKey, Cascade, Collection, ManyToOne} from "@mikro-orm/core";
+import { Reserva } from "../reserva/reserva.entity.js";
+
+@Entity()
+export class Cuota {
+
+  @ManyToOne(() => Reserva, {primary:true, cascade: [Cascade.ALL] })
+  reserva!: Reserva;
+
+  @PrimaryKey()
+  date!: number;
+
+  @Property()
+  monto!: number;
+
+  @Property()
+  estado!: 'PENDIENTE' | 'PAGADA';
+
 }
