eaeccc6ad8a0a7e7d5ee1c144f754cf70416884b
unknown <felipemuntaabski@gmail.com>
2025-10-13 16:56:04 -0300

    filtered cocheras

--- apps/backend/src/cochera/cochera.routes.ts
+++ apps/backend/src/cochera/cochera.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { findAll, findOne, add, update, remove, sanitizedCocheraInput } from "./cochera.controller.js"
 export const CocheraRouter = Router()
 CocheraRouter.get('/', findAll)
-CocheraRouter.get('/:numero',sanitizedCocheraInput, findOne)
+CocheraRouter.get('/:number',sanitizedCocheraInput, findOne)
 CocheraRouter.post('/', sanitizedCocheraInput, add);
-CocheraRouter.put('/:numero', sanitizedCocheraInput, update);
-CocheraRouter.delete('/:numero', remove);
+CocheraRouter.put('/:number', sanitizedCocheraInput, update);
+CocheraRouter.delete('/:number', remove);
